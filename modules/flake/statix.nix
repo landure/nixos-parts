39fda1442dd715d51d343d0032b3f84dd93dfdf7
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-04-17 09:31:59 +0200

    build(flake): 🔨 add statix check to flake check hook

---
+++ modules/flake/statix.nix
@@ -0,0 +1,66 @@
+/**
+  # nix-unit
+
+  ## 🛠️ Tech Stack
+
+  - [nix-unit homepage](https://nix-community.github.io/nix-unit/)
+    ([nix-unit @ GitHub](https://github.com/nix-community/nix-unit)).
+  - [flake-parts homepage](https://flake.parts/)
+    ([flake-parts @ GitHub](https://github.com/hercules-ci/flake-parts)).
+  - [flake-file homepage](https://flake-file.oeiuwq.com/)
+    ([flake-file @ GitHub](https://github.com/vic/flake-file)).
+
+  ## 🙇 Acknowledgements
+
+  - [Dendrix](https://dendrix.oeiuwq.com/index.html).
+*/
+{
+  inputs,
+  lib,
+  pkgs,
+  ...
+}:
+let
+  inherit (lib) mkDefault getExe;
+in
+{
+  flake-file = {
+    inputs = {
+      devshell.url = mkDefault "github:numtide/devshell";
+    };
+
+    check-hooks = [
+      {
+        index = 10;
+        program =
+          pkgs:
+          pkgs.writeShellApplication {
+            name = "statix-check";
+            text = "${getExe pkgs.statix} check";
+          };
+      }
+    ];
+  };
+
+  imports = [
+    (inputs.devshell.flakeModule or { })
+  ];
+
+  perSystem =
+    { pkgs, ... }:
+    {
+
+      devshells.default = {
+        packages = with pkgs; [ statix ];
+
+        commands = [
+          {
+            name = "statix-check";
+            help = "Lint nix files with statix";
+            command = "${getExe pkgs.statix} check";
+            category = "lint";
+          }
+        ];
+      };
+    };
+}
