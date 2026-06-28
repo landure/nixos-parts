a924de5cf68c36af0bf69bd2eeac808b08b33054
Pierre-Yves LANDURé <pierre-yves@landure.fr>
2026-06-28 10:03:30 +0200

    feat(nixos): :sparkles: add biapy.presets.system to NixOS modules

---
+++ modules/nixos/_biapy/presets/system.nix
@@ -0,0 +1,14 @@
+{ config, lib, ... }:
+let
+  inherit (lib.modules) mkDefault mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.biapy.presets.system;
+in
+{
+  options.biapy.presets.system.enable = mkEnableOption "Base system preset";
+
+  config = mkIf cfg.enable {
+    biapy.networking.ntp.enable = mkDefault true;
+  };
+}
