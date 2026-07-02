acace51c0dd6f66cc389438c799ba0f9dca27593
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-07-02 08:48:04 +0200

    feat(fzf): :sparkles: add tsukie.com scripts to programs.fzf module

--- modules/home/_biapy/programs/fzf.nix
+++ modules/home/_biapy/programs/fzf.nix
@@ -23,6 +23,7 @@
   ## 🙇 Acknowledgements
 
   - [FZF vs Skim (October 2025): which fuzzy finder should power your terminal? @ GitgasBlade](https://gigasblade.blogspot.com/2025/10/fzf-vs-skim-october-2025-which-fuzzy.html).
+  - [Use CLI like a modern tech bro @ tsukie.com](https://www.tsukie.com/en/technologies/use-cli-like-a-modern-tech-bro/).
 */
 {
   config,
@@ -31,18 +32,15 @@
   ...
 }:
 let
-  inherit (lib.meta) getExe;
+  inherit (lib.meta) getExe getExe';
   inherit (lib.modules) mkIf mkDefault;
   inherit (lib.options) mkEnableOption;
 
   cfg = config.biapy.programs.fzf;
-
 in
 {
   options = {
-    biapy.programs.fzf = {
-      enable = mkEnableOption "fzf";
-    };
+    biapy.programs.fzf.enable = mkEnableOption "fzf fuzzy finder";
   };
 
   config = mkIf cfg.enable {
@@ -53,6 +51,11 @@ in
 
       fzf = {
         enable = mkDefault true;
+
+        # Shell history and completion using fzf in OS.
+        keybindings = mkDefault true;
+        fuzzyCompletion = mkDefault true;
+
         defaultCommand = "${getExe config.programs.fd.package} --type 'f' --hidden --follow --exclude '.git'";
         defaultOptions = [
           "--height=40%"
@@ -73,6 +76,45 @@ in
           --bind='change:reload:rg --line-number --no-heading --color=always {q} || true' \
           --preview='${getExe config.programs.bat.package} --style=numbers --color=always --highlight-line {2} {1}' \
           --preview-window='right:70%:wrap'
+      '')
+
+      (pkgs.writeShellScriptBin "p" ''
+        # p: fzf + bat Preview (File Browser Mode).
+        # Scroll files and see live syntax-highlighted previews.
+        # Usage: `fd | p`
+        # see https://www.tsukie.com/en/technologies/use-cli-like-a-modern-tech-bro/
+        set -e
+        set -u
+        set -o pipefail
+
+        exec ${getExe config.programs.fzf.package} --preview "${getExe config.programs.bat.package} --color=always --style=numbers --line-range=':500' {}"
+      '')
+
+      (pkgs.writeShellScriptBin "v" ''
+        # v: fd + fzf + bat Fuzzy Open File with Preview.
+        # Scroll files and see live syntax-highlighted previews.
+        # Accepts fd arguments.
+        # Usage: `v -IH './vendor'`
+        # see https://www.tsukie.com/en/technologies/use-cli-like-a-modern-tech-bro/
+        set -e
+        set -u
+        set -o pipefail
+
+        ${getExe config.programs.fd.package} --type 'f' "''${@}" |
+        ${getExe config.programs.fzf.package} --preview "${getExe config.programs.bat.package} --color='always' --style='numbers' {}"
+      '')
+
+      (pkgs.writeShellScriptBin "killf" ''
+        # killf: Fuzzy Kill Process.
+        # see https://www.tsukie.com/en/technologies/use-cli-like-a-modern-tech-bro/
+        set -e
+        set -u
+        set -o pipefail
+
+        ps -ef |
+        ${getExe config.programs.fzf.package} --header "Select process to kill" |
+        ${getExe pkgs.gawk} '{print ''$2}' |
+        ${getExe' pkgs.uutils-findutils "xargs"} ${getExe' pkgs.uutils-coreutils-noprefix "kill"} -9
       '')
     ];
   };
