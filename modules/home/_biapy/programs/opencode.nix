3608af56940544009811f3ecadbdb2be03ef6261
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-08-05 10:40:39 +0200

    feat(opencode): :sparkles: add programs.opencode

---
+++ modules/home/_biapy/programs/opencode.nix
@@ -0,0 +1,40 @@
+/**
+  # OpenCode
+
+  ## 🛠️ Tech Stack
+
+  - [OpenCode homepage](https://opencode.ai/)
+    ([OpenCode @ GitHub](https://github.com/anomalyco/opencode)).
+
+  ## 📝 Documentation
+
+  ### 🏠 Home Manager
+
+  - [programs.opencode @ Home Manager](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.opencode.enable).
+*/
+{
+  config,
+  lib,
+  ...
+}:
+let
+  inherit (lib.modules) mkDefault mkIf;
+  inherit (lib.options) mkEnableOption;
+
+  cfg = config.biapy.programs.opencode;
+
+in
+{
+  options = {
+    biapy.programs.opencode = {
+      enable = mkEnableOption "opencode";
+    };
+  };
+
+  config = mkIf cfg.enable {
+    programs.opencode = {
+      enable = mkDefault true;
+      web.enable = mkDefault true;
+    };
+  };
+}
