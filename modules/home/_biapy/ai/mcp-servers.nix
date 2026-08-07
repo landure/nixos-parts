6b799bcd6a78463ee3d20c5c85e806b58bdd3857
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-08-07 12:11:36 +0200

    fix(ai.mcp-servers): :bug: fix module name and contents

--- modules/home/_biapy/ai/mcp-servers.nix
+++ modules/home/_biapy/ai/mcp-servers.nix
@@ -3,8 +3,10 @@
 
   ## 🛠️ Tech Stack
 
-  - [skills.sh homepage](https://www.skills.sh/).
-    ([skills @ GitHub](https://github.com/vercel-labs/skills)).
+  - [MCP Language Server @ GitHub](https://github.com/isaacphi/mcp-language-server).
+  - [MCP-NixOS homepage](https://mcp-nixos.io/).
+    ([MCP-NixOS @ GitHub](https://github.com/utensils/mcp-nixos)).
+  - [Model Context Protocol servers @ GitHub](https://github.com/modelcontextprotocol/servers).
 */
 {
   config,
@@ -17,18 +19,22 @@ let
   inherit (lib.modules) mkDefault mkIf;
   inherit (lib.options) mkEnableOption;
 
-  cfg = config.biapy.ai.agent-skills;
+  cfg = config.biapy.ai.mcp-servers;
 
 in
 {
   options = {
-    biapy.ai.agent-skills.enable = mkEnableOption "AI agents skills tools";
+    biapy.ai.mcp-servers.enable = mkEnableOption "MCP servers";
   };
 
   config = mkIf cfg.enable {
     home.packages = with pkgs; [
-      skills
-      ctx7
+      mcp-nixos
+      mcp-server-git
+      mcp-server-fetch
+      mcp-server-filesystem
+      mcp-language-server
+      context7-mcp
     ];
   };
 }
