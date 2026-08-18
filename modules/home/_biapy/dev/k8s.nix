9ec0682601f4f0ca1da653d9c0655e703cf59cee
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-08-18 10:26:19 +0200

    feat(home): :sparkles: add krew to dev.k8s

--- modules/home/_biapy/dev/k8s.nix
+++ modules/home/_biapy/dev/k8s.nix
@@ -4,24 +4,39 @@
   ## 🛠️ Tech Stack
 
   - [helm homepage](https://helm.sh/)
-    ([helm @ GitHub](https://github.com/helm/helm)).
+    ([helm @ GitHub](https://github.com/helm/helm))
+    is a tool for managing Charts.
+    Charts are packages of pre-configured Kubernetes resources.
   - [helm-ls @ GitHub](https://github.com/mrjosh/helm-ls)
+    is a Language server for Helm
   - [k9s homepage](https://k9scli.io/ )
-    ([k9s @ GitHub](https://github.com/derailed/k9s)).
-  - [kail @ GitHub](https://github.com/boz/kail).
+    ([k9s @ GitHub](https://github.com/derailed/k9s))
+    is a terminal based UI to interact with Kubernetes clusters.
+  - [kail @ GitHub](https://github.com/boz/kail)
+    is a kubernetes log viewer.
   - [kind homepage](https://kind.sigs.k8s.io/)
-    ([kind @ GitHub](https://github.com/kubernetes-sigs/kind)).
+    ([kind @ GitHub](https://github.com/kubernetes-sigs/kind))
+     is a tool for running local Kubernetes clusters using Docker container “nodes”.
+  - [krew homepage](https://krew.sigs.k8s.io/)
+    (krew @ GitHub)(https://github.com/kubernetes-sigs/krew)
+    is the plugin manager for `kubectl` command-line tool.
   - [kubecolor homepage](https://kubecolor.github.io/)
     ([kubecolor @ GitHub](https://github.com/kubecolor/kubecolor))
+     is a `kubectl` wrapper used to add colors to its output.
   - [kubectl @ GitHub](https://github.com/kubernetes/kubectl).
   - [ktop homepage](https://ktop.app/)
-    ([ktop @ GitHub](https://github.com/vladimirvivien/ktop)).
+    ([ktop @ GitHub](https://github.com/vladimirvivien/ktop))
+    is a top-like tool for Kubernetes cluster metrics.
   - [kty homepage](https://kty.dev/)
-    ([kty @ GitHub](https://github.com/grampelberg/kty)).
-  - [stern @ GitHub](https://github.com/stern/stern).
+    ([kty @ GitHub](https://github.com/grampelberg/kty))
+    is a terminal for Kubernetes.
+  - [stern @ GitHub](https://github.com/stern/stern)
+    provides multi pod and container log tailing for Kubernetes.
   - [talosctl homepage](https://www.siderolabs.com/talos-linux)
-    ([talosctl @ GitHub](https://github.com/siderolabs/talos/tree/main/cmd/talosctl)).
-  - [talos-pilot @ GitHub](https://github.com/Handfish/talos-pilot).
+    ([talosctl @ GitHub](https://github.com/siderolabs/talos/tree/main/cmd/talosctl))
+    is Talos Linux command-line tool.
+  - [talos-pilot @ GitHub](https://github.com/Handfish/talos-pilot)
+    is a Talos TUI for real-time node monitoring, log streaming, etcd health, and diagnostics.
 
   ## 📝 Documentation
 
@@ -67,6 +82,7 @@ in
         kail
         kind
         kubectl
+        krew
         ktop
         kty
         stern
