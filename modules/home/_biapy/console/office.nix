4b25b9219438ecf7c360f1e0a51fe2f1eed7327d
Pierre-Yves Landuré <py.landure@cobredia.fr>
2026-08-18 10:25:38 +0200

    feat(home): :sparkles: add ripgrep-all to console.office

--- modules/home/_biapy/console/office.nix
+++ modules/home/_biapy/console/office.nix
@@ -8,18 +8,20 @@
   - [dstask @ GitHub](https://github.com/naggie/dstask)
     is a personal task tracker designed to help you focus.
     It is similar to Taskwarrior but uses git to synchronise instead of a special protocol.
+  - [glow @ GitHub](https://github.com/charmbracelet/glow)
+    renders markdown on the CLI, with pizzazz! 💅🏻
   - [Joplin Terminal Application @ Joplin](https://joplinapp.org/help/apps/terminal/)
     is a note taking app.
   - [jrnl homepage](https://jrnl.sh/en/stable/)
     ([jrnl @ GitHub](https://github.com/jrnl-org/jrnl))
     is a journal application for the command line.
-  - [glow @ GitHub](https://github.com/charmbracelet/glow)
-    renders markdown on the CLI, with pizzazz! 💅🏻
-  - [Taskwarrior homepage](https://taskwarrior.org/)
-    ([Taskwarrior @ GitHub](https://github.com/GothenburgBitFactory/taskwarrior))
-    is a command line task list management utility.
+  - [ripgrep-all (rga) @ GitHub](https://github.com/phiresky/ripgrep-all)
+    is ripgrep, but also search in PDFs, E-Books, Office documents, `zip`, `tar.gz`, …
   - [rucola @ GitHub](https://github.com/Linus-Mussmaecher/rucola)
     is a terminal-based markdown note manager..
+    - [Taskwarrior homepage](https://taskwarrior.org/)
+      ([Taskwarrior @ GitHub](https://github.com/GothenburgBitFactory/taskwarrior))
+      is a command line task list management utility.
   - [tdf @ GitHub](https://github.com/itsjunetime/tdf)
     is a terminal-based PDF viewer.
 
@@ -27,8 +29,12 @@
 
   ### 🏠 Home Manager
 
-  - [programs.jrnl](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.jrnl.enable).
-  - [programs.taskwarrior](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.taskwarrior.enable).
+  - [programs.jrnl @ Home Manager](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.jrnl.enable).
+  - [programs.jrnl @ NixOS reference](https://search.nixos.org/options?source=home_manager&query=programs.jrnl.).
+  - [programs.ripgrep-all @ Home Manager](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.ripgrep-all.enable).
+  - [programs.ripgrep-all @ NixOS reference](https://search.nixos.org/options?source=home_manager&query=programs.ripgrep-all.).
+  - [programs.taskwarrior @ Home Manager](https://nix-community.github.io/home-manager/options.xhtml#opt-programs.taskwarrior.enable).
+  - [programs.taskwarrior @ NixOS reference](https://search.nixos.org/options?source=home_manager&query=programs.taskwarrior.).
 
   ## 🙇 Acknowledgements
 
@@ -67,7 +73,7 @@ in
 
     programs = {
       jrnl.enable = mkDefault true;
-
+      ripgrep-all.enable = mkDefault true;
       taskwarrior.enable = mkDefault true;
     };
 
